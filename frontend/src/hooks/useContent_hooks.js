2d7bb87d0ee4ec78e4285a6cda7f4c201a6d548f
Ttw27 <tim@hugehoods.co.uk>
2026-07-07 10:46:50 +0100

    Add files via upload

---
+++ frontend/src/hooks/useContent_hooks.js
@@ -0,0 +1,3 @@
+// Re-export from the main lib version
+// This allows Landing.jsx to import: import { useContent } from "../hooks/useContent"
+export { useContent, useAllContent, notifyContentChanged } from "../lib/useContent";
